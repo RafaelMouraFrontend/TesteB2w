b072425f759b885c6310111eb0ad91d0a94ce0b9
Rafael <rafael.mouraweb@gmail.com>
2019-06-01 15:07:19 -0300

    mais estrutura de paginas e rotas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from './screen/Home';
+import Categories from './screen/Categories';
+
+
+const ContainerIndex = styled.div `
+  background: #ccc;
+`;
 
 class App extends Component {
-  constructor () {
-    super()
-  }
+
 
   render(){
     return (
-      <ContainerIndex>
-         {props.children}
-      </ContainerIndex>
+      <Router>
+        <ContainerIndex>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/categories" component={Categories} />
+          
+        </Switch>
+        </ContainerIndex>
+      </Router>
     );
   }
 }
---
+++ src/screen/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../components/Logo/Logo';
+import ButtomStart from '../components/ButtonStart/ButtonStart';
+
+
+  const Home = () => (
+    <div>
+        <Logo />
+        <ButtomStart>
+            Start Game
+        </ButtomStart>
+    </div> 
+  );
+  
+export default Home
