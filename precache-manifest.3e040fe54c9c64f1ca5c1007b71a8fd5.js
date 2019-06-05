554a770ae57f960b1b9dd599d6e32835e22045fb
RafaelMouraFrontend <rafael.mouraweb@gmail.com>
2019-06-05 18:59:51 +0000

    Updates

---
+++ precache-manifest.3e040fe54c9c64f1ca5c1007b71a8fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ceac24d2006ccfad08627e8b3a57d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d6d15fa97dfee7efc1",
+    "url": "/static/js/2.cd7404c4.chunk.js"
+  },
+  {
+    "revision": "ab8594418a61537ff5e6",
+    "url": "/static/js/main.0ee3a7a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "779ca9da6b4f3656d289b82cbfa54ac3",
+    "url": "/static/media/load.779ca9da.gif"
+  },
+  {
+    "revision": "1575d93e79c113e964a538278a5dfd91",
+    "url": "/static/media/logo.1575d93e.svg"
+  },
+  {
+    "revision": "6b1dac0d58dc04813710f438f5ca1c66",
+    "url": "/static/media/planet-earth.6b1dac0d.svg"
+  }
+]);
