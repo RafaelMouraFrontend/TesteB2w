1c40735d82a78f84800d3fc52382dab35f2328e3
Rafael <rafael.mouraweb@gmail.com>
2020-11-22 15:06:45 -0300

    ajust logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import Logoimg from '../../assets/imgs/logo.svg'
 
+const ImgLogo = styled.img `
+  max-width: 600rem;
+  margin: 0 auto;
+`;
+
   const Logo = () => (
-    <img src={Logoimg} alt="Hapz"/>
+    <ImgLogo src={Logoimg} alt="StarWars"/>
   );
   
 export default Logo
